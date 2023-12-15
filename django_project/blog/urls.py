9f521331b4dd6454e315bc9b8cf3f20dafa7988b
KHOITRINH03 <Johntrinhnhat@gmail.com>
2023-12-14 20:04:30 -0500

    modify views.py

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns=[
     path("", views.home, name='blog-home'),
-    path("", views.about, name='blog-about'),
-
+    path("about", views.about, name='blog-about')
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
+posts=[
+    {
+        'title' : 'CS50SQL',
+        'content' : 'Introduction to Databases with SQL',
+        'author' : 'Carter Zenke',
+        'date_posted' : 'December 14, 2023'
+    },
+    {
+        'title' : 'CS50P',
+        'content' : 'Introduction to Programming with Python',
+        'author' : 'David Malan',
+        'date_posted' : 'December 15, 2023'
+    },
+    {
+        'title' : 'CS50Web',
+        'content' : 'Introduction to Web Programming with Python and Javascript',
+        'author' : 'Brian Yu',
+        'date_posted' : 'December 16, 2023'
+    }
+]
+
 # Create your views here.
+def home(request):
+    return render(request, "blog/home.html", {
+        "posts": posts
+    })
+
+def about(request):
+    return render(request, "blog/about.html", {
+        'title': 'About'
+    })
